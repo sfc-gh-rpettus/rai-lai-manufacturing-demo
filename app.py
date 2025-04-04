f4448a323fa59c78d11b43587806cb575fc74994
Randy Pettus <randy.pettus@snowflake.com>
2025-04-04 09:48:36 -0600

    not working code

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import uuid
 import warnings
 import requests
 import queue
+import random
 
 import streamlit as st
 
@@ -19,6 +20,18 @@ from agent_gateway import Agent
 from agent_gateway.tools import PythonTool
 from snowflake.cortex import Complete
 
+
+sys.path.append("./relationalai.zip")
+#imports = ('@rai_demo.public.packages/relationalai.zip')
+#sys.path.append('@rai_demo.public.packages/relationalai.zip')
+
+import relationalai as rai
+from relationalai.std import alias, aggregates, rel
+from relationalai.std.graphs import Graph
+
+# Get the current Snowflake session
+session = get_active_session()
+
 # App title and introduction
 st.title("🏭 Manufacturing Agent")
 st.markdown("""
@@ -26,10 +39,7 @@ st.markdown("""
 """)
 st.write("This is under active development.")
 
-# Get the current Snowflake session
-session = get_active_session()
-
-tab1, tab2 = st.tabs(["🗒 Instructions", "🤖 Chat with Agent"])
+tab1, tab2, tab3 = st.tabs(["🗒 Instructions", "🤖 Chat with Agent", "RAI Testing"])
 
 # -------------------------------
 # Defining functions (tools)
@@ -51,37 +61,165 @@ def landing_lens_summarize(input):
     logging.info("Running landing_lens_summarize tool")
     return Complete(model, f"#INSTRUCTIONS: Summarize if a defect was found based on the following contents. BE SURE TO INCLUDE THE FILE NAME! #CONTENTS:  {input}")
 
-def defect_recommendation(input):
-    """Create a summary including the results of the batch analysis"""
-    logging.info("Running defect_recommendation tool")
-    return Complete(model, f"#INSTRUCTIONS: Provide an impact analysis of this part if defective based on the batch count impact. If the count exceeds 20 say this needs to be FLAGGED!!!! {input}")
 
-def query_part():
-    """Query defective images to find the part number and operator"""
+def get_sku_site():
+    """Query defective images to find the site and part"""
     logging.info("Running query_part tool")
     sql_cmd = f"""
-    select * 
-    from RAI_DEMO.RAI_LAI_MANUFACTURING.MANUFACTURING_HISTORY 
-    where REGEXP_SUBSTR(FILENAME, '[^/]+/[^/]+$') = '{selected_image}' limit 1
+    select  sk.name as SKU_NAME, s.name AS SITE_NAME
+    from RAI_DEMO.RAI_LAI_MANUFACTURING.IMAGES i
+    left join RAI_DEMO.RAI_LAI_MANUFACTURING.SITE s on s.id = i.site
+    left join RAI_DEMO.RAI_LAI_MANUFACTURING.SKU sk on sk.id = i.sku
+    where i.file_key = '{selected_image}'
     """
     df = session.sql(sql_cmd).to_pandas()
     return df.to_dict()
 
-def query_casting_batch(casting_batch_id):
-    """Query to find all items associated with a casting batch id"""
-    logging.info("Running query_casting_batch tool")
-    sql = f"""
-        select count(*) from RAI_DEMO.RAI_LAI_MANUFACTURING.MANUFACTURING_HISTORY 
-        where casting_batch_id = '{casting_batch_id}'
-        and label_name = 'defects'
-        """
-    df = session.sql(sql).to_pandas()
-    return df.to_dict()
 
-def key_value_extractor(query_part_output):
+def key_value_extractor(dict_output):
     """Extract out the value associated with a key from a dictionary"""
     logging.info("Running key_value_extractor tool")
-    return Complete(model, f"#INSTRUCTIONS: You are to extract the casting_batch_id value from the following. DO NOT INLCUDE ANYTHING BUT THE value!!! #INPUT: {query_part_output}")
+    return Complete(model, f"#INSTRUCTIONS: You are to extract the sku name value from the following. Only include the value. #INPUT: {dict_output}")
+
+def insights_synthesis(input):
+    """Create a summary including the results of the batch analysis"""
+    logging.info("Running insights_synthesis tool")
+    return Complete(model, f"#INSTRUCTIONS: Provide an impact analysis of the impacted sites, locations etc based on the input. {input}")
+
+# -------------------------------
+# RAI configuration
+# -------------------------------
+config = rai.Config({})
+config.file_path = "__inline__"
+
+   
+#@st.cache_resource()
+def get_model():
+    model = rai.Model("SUPPLY_CHAIN_DEMO_CARS_WIP", config = config, connection=session, profile="sf-build-demo", isolated=True)
+    return model
+    
+
+model = get_model()
+
+sf_db = 'RAI_DEMO'
+sf_schema = 'RAI_LAI_MANUFACTURING'
+sf_namespace= f'{sf_db}.{sf_schema}'
+
+
+# %%
+Site = model.Type("Site", source=f"{sf_namespace}.SITE")
+Sku = model.Type("Sku", source=f"{sf_namespace}.SKU")
+SkuAtSite = model.Type("SkuAtSite", source=f"{sf_namespace}.SKU_AT_SITE")
+BomComponent = model.Type("BomComponent", source=f"{sf_namespace}.BILL_OF_MATERIALS")
+BomMaster = model.Type("BomMaster")
+Operation = model.Type("Operation", source=f"{sf_namespace}.OPERATION")
+
+#@st.cache_data
+def build_supplychain_kg(model, Site, Sku, SkuAtSite, BomComponent, BomMaster, Operation):
+
+    # Add Named Relations between SkuAtSite and SKU / Site
+    with model.rule():
+        entity = SkuAtSite()
+        entity.set(
+            for_sku=Sku(id=entity.sku_id),  # SkuAtSite.for_sku --> Sku    
+            at_site=Site(id=entity.site_id) # SkuAtSite.at_site --> Site
+        ) 
+
+    # Extract BomMaster from BILL_OF_MATERIALS, Connect BomMaster / BomComponent to Sku / Site, 
+    with model.rule():
+        bomComponent = BomComponent()
+        bomComponent.set(my_SkuAtSite = 
+            SkuAtSite(sku_id=bomComponent.input_sku_id, site_id=bomComponent.site_id))
+        bomMaster = BomMaster.add(my_SkuAtSite =
+            SkuAtSite(sku_id=bomComponent.output_sku_id, site_id=bomComponent.site_id))
+        # BomMaster.has_components --> BomComponent
+        bomMaster.has_components.extend([bomComponent])
+
+    # OPERATION Table --> Operation Entity
+    Operation = model.Type("Operation", source=f"{sf_namespace}.OPERATION")
+    with model.rule():
+        entity = Operation()
+        entity.set(output_SkuAtSite= 
+                    SkuAtSite(sku_id=entity.output_sku,
+                            site_id=entity.output_site_id))
+        with model.match():
+            with entity.type == "SHIP": # for Shipping
+                input_SkuAtSite=SkuAtSite(sku_id=entity.output_sku, site_id=entity.source_site_id)
+                entity.input_SkuAtSite.extend([input_SkuAtSite])
+            with entity.type == "SUPPLY": # for Shipping
+                input_SkuAtSite=SkuAtSite(sku_id=entity.output_sku, site_id=entity.source_site_id)
+                entity.input_SkuAtSite.extend([input_SkuAtSite])
+            with model.case(): # otherwise (Making or Packing)
+                # Link to BOM (transformation) and sourcelocation (transportation)
+                entity.set(has_bom=BomMaster(my_SkuAtSite=entity.output_SkuAtSite))
+                entity.input_SkuAtSite.extend([entity.has_bom.has_components.my_SkuAtSite])
+
+    static_graph = Graph(model)
+
+    # Project to a Static Supply Chain Graph for visualization and analytics
+    static_graph = Graph(model)
+
+    with model.rule(): # SkuAtSite as Nodes
+        entity = SkuAtSite()
+        static_graph.Node.add(entity)
+
+    with model.rule(): # Operation as Nodes
+        entity = Operation()
+        static_graph.Node.add(entity)
+
+    with model.rule(): # Input to Operation as Edges
+        entity = Operation()
+        static_graph.Edge.add(from_=entity.input_SkuAtSite, to=entity)
+
+    with model.rule(): # Operation to Output as Edge
+        entity = Operation()
+        static_graph.Edge.add(from_=entity, to=entity.output_SkuAtSite)
+
+    static_graph.fetch()
+
+    return static_graph
+
+#@st.cache_data
+def get_rai_impact(input): #, site_name):
+    logging.info("Running relationalAI query")
+    car_supply_chain_graph = build_supplychain_kg(model, Site, Sku, SkuAtSite, BomComponent, BomMaster, Operation)
+    # Where Used: raw material impacts in the supply chain 
+    with model.query() as select:  # [where_used]
+        # find sku with part name
+        sku = Sku(name='Brake Disc')
+        # find site with vendor name
+        site = Site(name='ZF')
+        # find all sku/site containging the sku
+        start_nodes = SkuAtSite(sku_id=sku.id, site_id=site.id)
+        # find all reachable nodes via built-in graph algorithm
+        reachable = car_supply_chain_graph.compute.reachable_from(start_nodes)
+        # only care about end points of a supply chain: sales regions
+        car_supply_chain_graph.compute.outdegree(reachable) == 0
+        # return impacted downstream Sku + Site node info
+        with SkuAtSite(reachable): 
+            result = select(reachable.sku_id, reachable.site_id,
+                            alias(reachable.for_sku.name, "Impacted Car"),
+                            alias(reachable.at_site.name, "Impacted Region"))
+
+    return result.results.to_dict()
+
+#@st.cache_data
+#def get_impact(input): #, site_name):
+#    sql_cmd = f"""
+#    select  *
+#    from RAI_DEMO.RAI_LAI_MANUFACTURING.SKU sk
+#    where sk.name = 'Brake Disc' 
+#    """
+#    result = session.sql(sql_cmd).to_pandas()
+#    return result.to_dict()
+
+def get_impact(input):
+    result = get_rai_impact("Brake Disc")
+    return result
+
+#result = get_impact("Brake Disc", "ZF")
+#st.write(get_impact("Brake Disc"))
+#st.write(result)  
 
 # -------------------------------
 # Configure and instantiate tools
@@ -99,44 +237,47 @@ ll_summarize_config = {
     "python_func": landing_lens_summarize,
 }
 
-defect_recommendation_config = {
-    "tool_description": "Provide an impact assessment for the part ONLY FOR defective parts. You will need to use the casting_batch_tool first and include results from other tools",
-    "output_description": "string of summarized analysis",
-    "python_func": defect_recommendation,
-}
-
-query_part_config = {
-    "tool_description": "query to find the part number, operator, and batch_id for the given image. You will use this to find the batch id associated with an image",
+get_sku_site_config = {
+    "tool_description": "query to find the sku and site. You will use this to extract the sku name and site name for insights",
     "output_description": "dict of summary data",
-    "python_func": query_part,
+    "python_func": get_sku_site,
 }
 
-casting_batch_config = {
-    "tool_description": "find count of items for a batch id. You will need to make sure to use the key_value_extract first to get the casting_batch_id value",
-    "output_description": "dict of data",
-    "python_func": query_casting_batch,
-}
-
-key_value_extract = {
-    "tool_description": "finds the value associated with a given key from the part output",
+key_value_config = {
+    "tool_description": "Extract out the value results",
     "output_description": "dict of data",
     "python_func": key_value_extractor,
 }
 
+get_impact_config = {
+    "tool_description": "finds the downstream supply chain count impact from a sku name.", # Get the sku name first from the key value config",
+    "output_description": "dict of data",
+    "python_func": get_impact,
+}
+
+insights_synthesis_config = {
+    "tool_description": "synthesize your findings of the impact from a dictionary", # Get the sku name first from the key value config",
+    "output_description": "string of your summary",
+    "python_func": insights_synthesis,
+}
+
+
 ll_inference_tool = PythonTool(**ll_config)
 ll_summarize_tool = PythonTool(**ll_summarize_config)
-defect_recommendation_tool = PythonTool(**defect_recommendation_config)
-query_part_tool = PythonTool(**query_part_config)
-casting_batch_tool = PythonTool(**casting_batch_config)
-key_value_extract_tool = PythonTool(**key_value_extract)
+get_sku_site_tool = PythonTool(**get_sku_site_config)
+key_value_extract_tool = PythonTool(**key_value_config)
+get_impact_tool = PythonTool(**get_impact_config)
+insights_synthesis_tool = PythonTool(**insights_synthesis_config)
+
 
 snowflake_tools = [
     ll_inference_tool, 
     ll_summarize_tool, 
-    defect_recommendation_tool, 
-    query_part_tool, 
-    casting_batch_tool,
-    key_value_extract_tool
+    get_sku_site_tool, 
+    #kkey_value_extract_tool,
+    get_impact_tool,
+    insights_synthesis_tool
+    
 ]
 
 # Create the agent
@@ -179,31 +320,22 @@ with tab2:
     # User Selections: Model & Image
     # -------------------------------
     
-    model = st.selectbox("Choose a model", 
-                         [
-                             'claude-3-5-sonnet',
-                             'snowflake-arctic',
-                             'mistral-large',
-                             'reka-flash',
-                             'llama2-70b-chat', 
-                             'mixtral-8x7b', 
-                             'mistral-7b',
-                         ]
-    )
+    model = 'claude-3-5-sonnet'
     
-    selected_image = st.selectbox("Select an image",
-                                 [
-                                     'defects/cast_def_0_0.jpeg',
-                                     'defects/cast_def_0_105.jpeg',
-                                     'defects/cast_def_0_107.jpeg',
-                                     'defects/cast_def_0_11.jpeg',
-                                     'defects/cast_def_0_110.jpeg',
-                                     'defects/cast_def_0_118.jpeg',
-                                     'defects/cast_def_0_133.jpeg',
-                                     'defects/cast_def_0_144.jpeg',
-                                     'defects/cast_def_0_148.jpeg',
-                                     'no-defects/cast_ok_0_102.jpeg'
-                                 ]
+    selected_image = st.selectbox(
+        "Select an image",
+        [
+            'defects/cast_def_0_0.jpeg',
+            'defects/cast_def_0_105.jpeg',
+            'defects/cast_def_0_107.jpeg',
+            'defects/cast_def_0_11.jpeg',
+            'defects/cast_def_0_110.jpeg',
+            'defects/cast_def_0_118.jpeg',
+            'defects/cast_def_0_133.jpeg',
+            'defects/cast_def_0_144.jpeg',
+            'defects/cast_def_0_148.jpeg',
+            'no-defects/cast_ok_0_102.jpeg'
+        ]
     )
     
     # Build the file path using the selected image
@@ -335,3 +467,4 @@ with tab2:
             else:
                 st.markdown(final_response)
                 st.session_state.messages.append({"role": "assistant", "content": final_response})
+
